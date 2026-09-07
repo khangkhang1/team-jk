c5242a36dbaeec75860a99a7c3ee1ffdc466e498
오윤섭 <dhdbstjq01@gmail.com>
2026-09-07 09:59:56 +0900

    dto 변수 순서 수정(date <-> type)

--- src/main/java/dto/PaymentDto.java
+++ src/main/java/dto/PaymentDto.java
@@ -4,8 +4,8 @@ public class PaymentDto {
 	
 /*
 	* payment_id	: 결제 id(pk)
-	* reservation_id: 결제 id(예약 fk)
-	* payment_amout	: 결제 금액
+	* reservation_id: 예약 id(예약 fk)
+	* payment_amount: 결제 금액
 	* payment_method: 결제 수단(카카오페이, 네이버페이 등)
 	* payment_date	: 결제 일자(시간 포함)
 	* payment_type	: 결제 유형(초기 예약(+), 취소(-))
@@ -15,13 +15,13 @@ public class PaymentDto {
 	
 	//결제 시 생성자
 	public PaymentDto(int payment_id, int reservation_id, int payment_amount, String payment_method,
-			String payment_date, String payment_type) {
+				String payment_type, String payment_date) {
 		this.payment_id 	= payment_id;
 		this.reservation_id = reservation_id;
 		this.payment_amount = payment_amount;
 		this.payment_method = payment_method;
-		this.payment_date 	= payment_date;
 		this.payment_type 	= payment_type;
+		this.payment_date 	= payment_date;
 	}
 
 	public int getPayment_id() {
@@ -40,14 +40,12 @@ public class PaymentDto {
 		return payment_method;
 	}
 
-	public String getPayment_date() {
-		return payment_date;
-	}
-
 	public String getPayment_type() {
 		return payment_type;
 	}
 	
-	
+	public String getPayment_date() {
+		return payment_date;
+	}
 	
 }
