1d748dc9754dadd2d182dec12921b447d887097d
임정규 <wjdrb1226@gmail.com>
2026-09-09 10:03:15 +0900

    ddd

--- src/main/java/common/DBConnection.java
+++ src/main/java/common/DBConnection.java
@@ -21,7 +21,7 @@ public class DBConnection {
 			e.printStackTrace();
 		}
 		String db_url = "jdbc:oracle:thin:@jsl-704:1523/xe";
-		String db_user = "team27_jk";      // TODO: 팀 DB 계정 확정되면 교체
+		String db_user = "icn_parking";      // TODO: 팀 DB 계정 확정되면 교체
 		String db_passward = "1234";       // TODO: 팀 DB 비밀번호 확정되면 교체
 
 		try {
